c22a4fdb8c7f5d25b3cc01793de5b82822d78980
YTZ09457 <83584355+YTZ09457@users.noreply.github.com>
2021-05-11 15:31:22 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://ytz09457.github.io/aifrog10832607/model.json';
 
 
 function preload() {
